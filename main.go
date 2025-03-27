c1d83257d9b495ca75cadd5e5f174eccb507f7fa
tiaszz <iskiizeh3@gmail.com>
2025-03-27 17:50:40 -0300

    setting the project up

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/tiaszz/hotel-reservation/api"
+)
+
+func main() {
+	listenAddr := flag.String("listenAddr", ":5000", "The listen address of the API server")
+	flag.Parse()
+
+	app := fiber.New()
+	apiv1 := app.Group("/api/v1")
+
+	apiv1.Get("/user", api.HandleGetUsers)
+    apiv1.Get("/user/id", api.HandleGetUser)
+	app.Listen(*listenAddr)
+}
